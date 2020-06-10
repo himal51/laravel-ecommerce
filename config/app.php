12c1061307ffd12da12c09f07880a7af56da3b66
Himal Majumder <himalmajumder@gmail.com>
2020-06-11 00:28:44 +0600

    laravel-debugbar added

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'LLC Ecommerce'),
+    'name' => env('APP_NAME', 'Laravel Ecommerce'),
 
     /*
     |--------------------------------------------------------------------------
