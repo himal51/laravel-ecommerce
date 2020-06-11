8aaca2ecffe77f831b86bdde8af023944da6adc0
Himal Majumder <himalmajumder@gmail.com>
2020-06-11 23:48:38 +0600

    models re-organized & ide-helper configured for models

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.14.1 on 2020-06-11 10:36:32.
+ * Generated for Laravel 7.14.1 on 2020-06-11 23:28:31.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
