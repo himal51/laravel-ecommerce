a66d3026566a103828266f55a76a2382ec4537c6
Himal Majumder <himalmajumder@gmail.com>
2020-06-14 22:11:24 +0600

    Products table seeder added

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Product::class, 20)->create();
+
+        $products = Product::select('id')->get();
+
+        foreach ($products as $product) {
+            $product->addMediaFromUrl('https://lorempixel.com/640/480/?87765')->toMediaCollection('products');
+        }
+    }
+}
